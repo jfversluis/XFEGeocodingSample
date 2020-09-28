d9ce7b01cf23247dbddd11bafa10cf9e1b969f27
Gerald Versluis <gerald@verslu.is>
2020-09-28 11:35:47 +0200

    Initial commit

---
+++ XFEGeocoderSample/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XFEGeocoderSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
